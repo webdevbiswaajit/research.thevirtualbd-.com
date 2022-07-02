f1f59b4a3240c5d03f8aaf8be2ca1e719bb3b228
Biswajit sikdar <biswajitsikdar51@gmail.com>
2022-07-02 17:54:02 +0600

    Our recharcher Page

--- js/coustom.js
+++ js/coustom.js
@@ -91,3 +91,41 @@ var swiper = new Swiper(".recharchSwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+// seemore seeless
+$(document).ready(function () {
+  var list = $(".partners__ul li");
+  var numToShow = 10;
+  var button = $(".partners__button__a");
+  var numInList = list.length;
+  var isShowing = true;
+  list.hide();
+  if (numInList > numToShow) {
+    button.show();
+  }
+  list.slice(0, numToShow).show();
+  button.click(function () {
+    var showing = list.filter(":visible").length;
+    if (isShowing) {
+      list.slice(showing - 1, showing + numToShow).fadeIn(100, onFadeComplete);
+    } else {
+      list.slice(showing - numToShow, numInList).fadeOut(100, onFadeComplete);
+    }
+  });
+
+  function onFadeComplete() {
+    var nowShowing = list.filter(":visible").length;
+
+    if (nowShowing == numInList && isShowing) {
+      isShowing = false;
+      button.text("Show less");
+    } else if (isShowing) {
+      button.text("Show more");
+    }
+
+    if (nowShowing == numToShow) {
+      button.text("Show more");
+      isShowing = true;
+    }
+  }
+});
